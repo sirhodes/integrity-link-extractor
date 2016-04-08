bfac4e53e5b3319780c2b544f3bd1d5770d785e3
Seth Rhodes <seth@thinkpixbit.com>
2016-04-07 23:59:55 -0400

    test: async test with test data! :tada:

--- test.js
+++ test.js
@@ -8,3 +8,8 @@ test('linkExtractor function exists', t => {
 test('linkExtractor takes 1 arguments', t => {
   t.is(linkExtractor.length, 1);
 });
+
+test('linkExtractor can parse the test file and return 13 links', async t => {
+  const results = await linkExtractor('./test-data.csv');
+  t.is(results.length, 13);
+});
